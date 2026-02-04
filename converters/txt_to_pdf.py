c88e2e5e067c949e1f56c9b813f1e4e6701a3797
viniciussavgs <deviniciusvgs@outlook.com>
2026-02-03 22:23:14 -0300

    style: melhora layout e formatação do pdf

--- converters/txt_to_pdf.py
+++ converters/txt_to_pdf.py
@@ -2,11 +2,18 @@ from fpdf import FPDF
 
 def txt_para_pdf(caminho_txt, caminho_pdf):
     pdf = FPDF()
+    pdf.set_auto_page_break(auto=True, margin=15)
     pdf.add_page()
+
+    #TÍTULO
+    pdf.set_font("Arial", style="B", size=16)
+    pdf.cell(0, 10, txt="Conversão de TXT para PDF", ln=True, align="C")
+    pdf.ln(5)
+    
+    #CORPO DO TEXTO
     pdf.set_font("Arial", size=12)
 
     with open(caminho_txt, "r", encoding="utf-8") as arquivo:
         for linha in arquivo:
-            pdf.cell(0, 8, txt=linha.strip(), ln=True)
-
+            pdf.multi_cell(0, 8, linha)
     pdf.output(caminho_pdf)
